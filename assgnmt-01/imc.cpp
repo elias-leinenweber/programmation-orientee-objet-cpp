e0eb819fce17840db2ccffe229515f56ade3d079
Elias Leinenweber <anschrslntxtr@gmail.com>
2020-06-24 21:38:06 +0200

    [assgnmt-01] code fourni au départ

---
+++ assgnmt-01/imc.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+/*****************************************************
+ * Compléter le code à partir d'ici
+ *****************************************************/
+
+
+/*******************************************
+ * Ne rien modifier après cette ligne.
+ *******************************************/
+
+int main()
+{
+  Patient quidam;
+  double poids, taille;
+  do {
+    cout << "Entrez un poids (kg) et une taille (m) : ";
+    cin >> poids >> taille;
+    quidam.init(poids, taille);
+    quidam.afficher();
+    cout << "IMC : " << quidam.imc() << endl;
+  } while (poids * taille != 0.0);
+  return 0;
+}
