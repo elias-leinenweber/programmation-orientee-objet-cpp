aa0d347eafa4fff2b5be9e3ceb0fa2b7546324d5
Elias Leinenweber <anschrslntxtr@gmail.com>
2020-06-26 11:51:53 +0200

    [assgnmt-04] implémenté `Creature', `Dragon', `Hydre' et `combat'

--- assgnmt-04/dragons.cpp
+++ assgnmt-04/dragons.cpp
@@ -13,16 +13,139 @@ class Creature
   /*****************************************************
    * Compléter le code à partir d'ici
    *****************************************************/
+protected:
+  string nom_;
+  int niveau_;
+  int points_de_vie_;
+  int force_;
+  int position_;
 
-         << ", niveau: "
-         << ", points de vie: "
-         << ", force: "
-         << ", points d'attaque: "
-         << ", position: "
-
-         << " n'est plus !"
-
+public:
+  Creature(string, int, int, int, int);
+  bool vivant() const;
+  int points_attaque() const;
+  void deplacer(int);
+  void adieux() const;
+  void faiblir(int);
+  void afficher() const;
+  int position() const;
 };
+
+Creature::Creature(string nom, int niveau, int points_de_vie, int force, int position = 0)
+: nom_(nom), niveau_(niveau), points_de_vie_(points_de_vie), force_(force), position_(position)
+{}
+
+bool
+Creature::vivant() const
+{
+  return points_de_vie_ > 0;
+}
+
+int
+Creature::points_attaque() const
+{
+  return vivant() ? niveau_ * force_ : 0;
+}
+
+void
+Creature::deplacer(int distance)
+{
+  position_ += distance;
+}
+
+void
+Creature::adieux() const
+{
+  cout << nom_ << " n'est plus !" << endl;
+}
+
+void
+Creature::faiblir(int points)
+{
+  points_de_vie_ -= points;
+  if (!vivant()) {
+    points_de_vie_ = 0;
+    adieux();
+  }
+}
+
+void
+Creature::afficher() const
+{
+  cout << nom_ << ", niveau: " << niveau_ << ", points de vie: " <<
+      points_de_vie_ << ", force: " << force_ << ", points d'attaque: " <<
+      points_attaque() << ", position: " << position_ << endl;
+}
+
+int
+Creature::position() const
+{
+  return position_;
+}
+
+class Dragon : public Creature {
+private:
+  int portee_flamme_;
+
+public:
+  Dragon(string, int, int, int, int, int);
+  void voler(int);
+  void souffle_sur(Creature &);
+};
+
+Dragon::Dragon(string nom, int niveau, int points_de_vie, int force, int portee_flamme, int position = 0)
+: Creature(nom, niveau, points_de_vie, force, position), portee_flamme_(portee_flamme)
+{}
+
+void
+Dragon::voler(int pos)
+{
+  position_ = pos;
+}
+
+void
+Dragon::souffle_sur(Creature &bete)
+{
+  int d;
+
+  d = distance(position_, bete.position());
+  if (vivant() && bete.vivant() && d <= portee_flamme_) {
+    bete.faiblir(points_attaque());
+    faiblir(d);
+    if (vivant() && !bete.vivant())
+      ++niveau_;
+  }
+}
+
+class Hydre : public Creature {
+private:
+  int longueur_cou_;
+  int dose_poison_;
+
+public:
+  Hydre(string, int, int, int, int, int, int);
+  void empoisonne(Creature &);
+};
+
+Hydre::Hydre(string nom, int niveau, int points_de_vie, int force, int longueur_cou, int dose_poison, int position = 0)
+: Creature(nom, niveau, points_de_vie, force, position), longueur_cou_(longueur_cou), dose_poison_(dose_poison)
+{}
+
+void
+Hydre::empoisonne(Creature &bete)
+{
+  if (vivant() && bete.vivant() && distance(position_, bete.position()) <= longueur_cou_) {
+    bete.faiblir(points_attaque() + dose_poison_);
+    if (!bete.vivant())
+      ++niveau_;
+  }
+}
+
+void combat(Dragon &dragon, Hydre &hydre)
+{
+  hydre.empoisonne(dragon);
+  dragon.souffle_sur(hydre);
+}
 /*******************************************
  * Ne rien modifier après cette ligne.
  *******************************************/
